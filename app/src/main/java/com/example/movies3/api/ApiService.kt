e87e8138b34df4c9117cc7d6325908f17683f46f
AleksandrIsakovAbakan <116267686+AleksandrIsakovAbakan@users.noreply.github.com>
2025-05-23 19:50:09 +0700

    Update ApiService.kt

--- app/src/main/java/com/example/movies3/api/ApiService.kt
+++ app/src/main/java/com/example/movies3/api/ApiService.kt
@@ -11,21 +11,21 @@ interface ApiService {
     @GET("movie?field=rating.kp&search=1-10&sortField=votes.kp&sortType=-1&limit=40")
     @Headers(
         "Accept: application/json",
-        "X-API-KEY: T4VWX6K-N86M3F5-Q6ZCCD6-T11T1PX"
+        "X-API-KEY: XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX"
     )
     fun loadMovies(@Query("page") page: Int): Single<MovieRs>
 
     @GET("movie/{idFilms}")
     @Headers(
         "Accept: application/json",
-        "X-API-KEY: T4VWX6K-N86M3F5-Q6ZCCD6-T11T1PX"
+        "X-API-KEY: XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX"
     )
     fun getTrailers(@Path("idFilms") idFilms: Int): Single<Doc>?
 
     @GET("review?page=1&limit=10&sortField=date&sortType=-1")
     @Headers(
         "Accept: application/json",
-        "X-API-KEY: T4VWX6K-N86M3F5-Q6ZCCD6-T11T1PX"
+        "X-API-KEY: XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX"
     )
     fun getReviews(@Query("movieId") idFilms: Int): Single<ReviewRs>?
 
